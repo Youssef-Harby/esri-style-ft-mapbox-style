23d17f1ca06bf0c5fa5f97c4be9b3ac8b0970865
Youssef-Harby <youssef_harby@yahoo.com>
2024-04-11 17:10:54 +0200

    Update maplibregl RTL text plugin initialization in main.ts

--- example/src/main.ts
+++ example/src/main.ts
@@ -5,8 +5,7 @@ import maplibregl from "maplibre-gl";
 // Set the RTL text plugin once at the application level
 maplibregl.setRTLTextPlugin(
   "https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js",
-  null, // Set the second argument to `false` to avoid throwing an error if called multiple times
-  true  // This enables the plugin to be lazy loaded when needed
+  true
 );
 
 const app = new App({
